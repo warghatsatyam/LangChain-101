23e640500671aa62cf995438ba14d191f8d3efdd
Satyam Warghat <warghatsatyam33@gmail.com>
2024-04-29 15:08:07 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+from langchain.llms import OpenAI
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.9)
+
+text = "Suggest a personalized workout routine for someone looking to improve cardiovascular endurance and prefers outdoor activities."
+print(llm(text))
